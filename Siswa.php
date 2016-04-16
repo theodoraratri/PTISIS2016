a694d7ae67b6cb3f2538625736989e4a0f4e1331
theodoraratri <theodoraratri@yahoo.co.id>
2016-04-16 14:40:47 +0700

    Update Siswa.php

--- Siswa.php
+++ Siswa.php
@@ -13,37 +13,37 @@
  */
 class Siswa extends CI_Model {
 
-    //put your code here
-    public $nomorinduk;
-    public $namasiswa;
-    public $keterangan;
-
     function __construct() {
         parent::__construct();
     }
 
-    function insertdatasiswa($data) {
+    function insertdatasiswa($data) {//function untuk memasukkan data siswa
         $this->db->insert('SISWA', $data);
     }
 
-    function updateDataSiswa($nomorinduk, $data) {
-        $this->db->where('nomorinduk', $nomorinduk);
+    function updatedatasiswa($data) {//function untuk mengedit data siswa
+        $this->db->where('nomorinduk', $data['nomorinduk']);
         $this->db->update('SISWA', $data);
     }
 
-    function deleteDataSiswa($nomorinduk) {
-        $this->db->where('nomorinduk', $nomorinduk);
-        $this->db->delete('agenda');
+    function deletedatasiswa($nomorinduk) {//function untuk menghapus data siswa
+         $this->db->delete('SISWA', $nomorinduk);
     }
 
-    function select_all() {
-        $this->db->select('*');
-        $this->db->from('SISWA');
-        $this->db->order_by('nomorinduk', 'desc');
+    public function tampilSiswa() {
+        $sql = "select * from SISWA ORDER BY NOMORINDUK DESC ";
+        return $sql->result();
+    }
+
+    function getAllSiswa() {
+        //select semua data yang ada pada table siswa $this--->db->select("*");
+        $this->db->from("SISWA");
         return $this->db->get();
     }
-
     function getSiswa($nomorinduk) {
-        $this->db->select('siswa', $nomorinduk);
+        $this->db->where('nomorinduk', $nomorinduk);
+        return $this->db->get('SISWA');
+        //select siswa berdasarkan id yang dimiliki
     }
+
 }
