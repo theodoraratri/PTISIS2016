bf2d3e97ca251abbd16cc439dd2c050e46f03d5b
suryamaharanidewi <suryamaharanidewi@gmail.com>
2016-04-20 05:44:03 +0700

    Added files via upload

--- DataGuru.php
+++ DataGuru.php
@@ -15,51 +15,101 @@ class DataGuru extends CI_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model("Guru"); //constructor yang dipanggil ketika memanggil guru.php untuk melakukan pemanggilan pada model : guru_model.php yang ada di folder models
+        $this->load->model("Guru"); 
+//constructor yang dipanggil ketika memanggil DataGuru.php untuk melakukan pemanggilan pada model : Guru.php yang ada di folder models
+    }
+
+    public function tampilKelola() {
+        $this->load->view('KelolaDataGuru');
+    }
+
+    public function tampilMasuk() {
+        $this->load->view('MasukDataGuru');
+    }
+
+    public function tampilHapus() {
+        $this->load->view('HapusDataGuru');
+    }
+
+    public function tampilEdit() {
+        $this->load->view('EditDataGuru');
     }
 
     public function index() {
         //Function yang digunakan untuk menampilkan view guru_view.php
-        $this->load->view('MasukDataGuru'); //menampilkan view 'guru_view' dan juga passing data dengan nama $data(Bentuknya array) yang berisi 'listGuru'
-    }
-
-    function guru()
-    {
-        $this->load->model('Guru');
-        $data['MasukDataGuru'] = $this->Guru->getGuru();
+//        $this->load->view('MasukDataGuru'); 
+//menampilkan view 'guru_view' dan juga passing data dengan nama $data(Bentuknya array) yang berisi 'listGuru'
+        $query = $this->db->get("guru");
+        $data['records'] = $query->result();
+        $this->load->helper('url');
         $this->load->view('MasukDataGuru', $data);
     }
-    
+
+    function getguru_hapus() {
+        $this->load->model('Guru');
+        $query = $this->Guru->getAllGuru();
+        $data['dguru'] = $query->result();
+        $this->load->view('HapusDataGuru', $data);
+    }
+
+    function getguru_masuk() {
+        $this->load->model('Guru');
+        $query = $this->Guru->getAllGuru();
+        $data['iguru'] = $query->result();
+        $this->load->view('MasukDataGuru', $data);
+    }
+
+    function getguru_edit() {
+        $this->load->model('Guru');
+        $query = $this->Guru->getAllGuru();
+        $data['eguru'] = $query->result();
+        $this->load->view('EditDataGuru', $data);
+    }
+
     public function insertDataGuru() {
         $data = array(
             'nip' => $this->input->post('nipe'),
             'namaguru' => $this->input->post('nmguru'),
             'password' => $this->input->post('passguru')
         );
-        $this->Guru->insertdataguru($data); //passing variable $data ke guru_model
-        $this->load->view('MasukDataGuru',$data);
-//        redirect('guru'); //redirect page ke halaman utama controller guru
+        $this->Guru->insertdataguru($data);
+        $this->load->helper('url');
+//        $this->load->view('MasukDataGuru', $data);
+        redirect('DataGuru/getguru_masuk'); //redirect page ke halaman utama controller guru
         //Function yang dipanggil ketika ingin memasukan guru ke dalam database
     }
 
-    public function updateDataGuru($nipe) {
+    public function updateDataGuru() {
         $nipe = array(
             'nip' => $this->input->post('nipe'),
             'namaGuru' => $this->input->post('nmguru'),
-            'password' => $this->input->post('pass')
+            'password' => $this->input->post('passguru')
         );
         $this->Guru->updatedataguru($nipe); //passing variable $id ke guru_model
 //        redirect('guru');
-        $this->load->view('EditDataGuru');
+//        $this->load->view('EditDataGuru');
+        redirect('DataGuru/getguru_edit');
         //Function yang dipanggil ketika ingin melakukan update terhadap produk yang ada di dalam database
     }
 
     public function deleteDataGuru($nipe) {
-        $where = array('nip' =>$nipe);
+        $where = array('nip' => $nipe);
         $this->Guru->deletedataguru($where, $nipe); //passing variable $id ke guru_model
-        $this->load->view('HapusDataGuru');
-//        redirect('guru');
+        $this->load->helper('url');
+//        $this->load->view('HapusDataGuru');
+        redirect('DataGuru/getguru_hapus');
         //Function yang dipanggil ketika ingin melakukan delete produk dari database
+    }
+
+    public function editform($nip) {
+
+        $query = $this->Guru->getGuru($nip);
+        foreach ($query->result() as $guru) {
+            $data['nip'] = $guru->nip;
+            $data['namaguru'] = $guru->namaguru;
+            $data['password'] = $guru->password;
+        }
+        $this->load->view('EditGuru', $data);
     }
 
 }
