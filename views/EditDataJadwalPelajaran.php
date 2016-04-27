190c3562d70744e3fd7e6d0b53230be6d88b7074
nikomangasih <nikomangasih@gmail.com>
2016-04-27 09:39:06 +0700

    Added files via upload

--- views/EditDataJadwalPelajaran.php
+++ views/EditDataJadwalPelajaran.php
@@ -17,108 +17,103 @@ and open the template in the editor.
     </head>
     <body>
         <!-- ####################################################################################################### -->
-        <!-- menampilkan kode jadwal,Nama kelas,Hari,Jam,NIP,mata pelajaran dari database dan untuk menekan 
-        tombol edit -->
-        
         <div class="wrapper col1">
             <div id="header">
                 <div id="logo">
-                    <h1><a href="HalamanAdmin.php">Sistem Informasi Siswa</a></h1>
+                    <h1><?php echo anchor('Kelola/indexAdmin/', 'Sistem Informasi Siswa'); ?></h1>
                     <p>SMA Pangudi Luhur Yogyakarta</p>
                 </div>
                 <div class="fl_right">
-                    
-                    <p>Tel: xxxxx xxxxxxxxxx | Mail: info@domain.com</p>
-                </div>
+                    <p>Tel: xxxxx xxxxxxxxxx | Mail: info@domain.com |<?php echo anchor('Kelola/index/', 'LOGOUT'); ?>| </p>   </div>
                 <br class="clear" />
             </div>
         </div>
         <!-- ####################################################################################################### -->
         <div class="wrapper col2">
-  <div id="topnav">
-    <ul>
-        <li><a href="HalamanAdmin.php">Halaman Admin</a>
-        <ul>
-        </ul>
-      </li>
-      <li><a href="KelolaDataSiswa.php">Kelola Data Siswa</a>
-        <ul>
-            <li><a href="MasukDataSiswa.php">Masukkan Data Siswa</a></li>
-            <li><a href="HapusDataSiswa.php">Menghapus Data Siswa</a></li>
-            <li class="last"><a href="EditDataSiswa.php">Mengedit Data Siswa</a></li>
-        </ul>
-      </li>
-      <li><a href="KelolaDataGuru.php">Kelola Data Guru</a>
-        <ul>
-            <li><a href="MasukDataGuru.php">Masukkan Data Guru</a></li>
-            <li><a href="HapusDataGuru.php">Menghapus Data Guru</a></li>
-            <li class="last"><a href="EditDataGuru.php">Mengedit Data Guru</a></li>
-       </ul>
-      </li>
-       <li><a href="KelolaDataKelas.php">Kelola Data Kelas</a>
+            <div id="topnav">
+                <ul>
+                    <li><?php echo anchor('Kelola/indexAdmin/', 'Halaman Admin'); ?>
                         <ul>
-                            <li><a href="MasukDataKelas.php">Masukkan Data Kelas</a></li>
-                            <li><a href="HapusDataKelas.php">Menghapus Data Kelas</a></li>
-                            <li class="last"><a href="EditDataKelas.php">Mengedit Data Kelas</a></li>
                         </ul>
                     </li>
-      <li class="active"><a href="KelolaJadwalPelajaran.php">Kelola Data Jadwal Pelajaran</a><ul>
-              <li><a href="MasukDataJadwalPelajaran.php">Masukkan Data Jadwal Pelajaran</a></li>
-              <li><a href="HapusDataJadwalPelajaran.php">Menghapus Data Jadwal Pelajaran</a></li>
-              <li class="last"><a href="EditDataJadwalPelajaran.php">Mengedit Data Jadwal Pelajaran</a></li>
-       </ul>
-      </li>
-    </ul>
-  </div>
-</div>
+                    <li><?php echo anchor('DataGuru/index/', 'Kelola Data Guru'); ?>
+                        <ul>
+                            <li><?php echo anchor('DataGuru/tampilmasuk_guru/', 'Masukkan Data Guru'); ?></li>
+                            <li><?php echo anchor('DataGuru/tampilhapus_guru/', 'Menghapus Data Guru'); ?></li>
+                            <li class="last"><?php echo anchor('DataGuru/tampiledit_guru/', 'Mengedit Data Guru'); ?></li>
+                        </ul>
+                    </li>
+                    <li><?php echo anchor('DataKelas/index/', 'Kelola Data Kelas'); ?>
+                        <ul>
+                            <li><?php echo anchor('DataKelas/tampilmasuk_kelas/', 'Masukkan Data Guru'); ?></li>
+                            <li><?php echo anchor('DataKelas/tampilhapus_kelas/', 'Menghapus Data Guru'); ?></li>
+                            <li class="last"><?php echo anchor('DataKelas/tampiledit_kelas/', 'Mengedit Data Guru'); ?></li>
+                        </ul>
+                    </li>
+                    <li><?php echo anchor('DataSiswa/index/', 'Kelola Data Siswa'); ?>
+                        <ul>
+                            <li><?php echo anchor('DataSiswa/tampilmasuk_siswa/', 'Masukkan Data Siswa'); ?></li>
+                            <li><?php echo anchor('DataSiswa/tampilhapus_siswa/', 'Menghapus Data Siswa'); ?></li>
+                            <li class="last"><?php echo anchor('DataSiswa/tampiledit_siswa/', 'Mengedit Data Siswa') ?></li>
+                        </ul>
+                    </li>
+                    <li><?php echo anchor('DataMataPelajaran/index/', 'Kelola Data Mata Pelajaran'); ?>
+                        <ul>
+                            <li><?php echo anchor('DataMataPelajaran/tampilmasuk_mapel/', 'Masukkan Data Mata Pelajaran'); ?></li>
+                            <li><?php echo anchor('DataMataPelajaran/tampilhapus_mapel/', 'Menghapus Data Mata Pelajaran'); ?></li>
+                            <li class="last"><?php echo anchor('DataMataPelajaran/tampiledit_mapel/', 'Mengedit Data Mata Pelajaran') ?></li>
+                        </ul>
+                    </li>
+                    <li class="active"><?php echo anchor('DataJadwalPelajaran/index/', 'Kelola Data Jadwal Pelajaran'); ?>
+                        <ul>
+                            <li><?php echo anchor('DataJadwalPelajaran/tampilmasuk_jadwalpelajaran/', 'Masukkan Data Jadwal Pelajaran'); ?></li>
+                            <li><?php echo anchor('DataJadwalPelajaran/tampilhapus_jadwalpelajaran/', 'Menghapus Data Jadwal Pelajaran'); ?></li>
+                            <li class="last"><?php echo anchor('DataJadwalPelajaran/tampiledit_jadwalpelajaran/', 'Mengedit Data Jadwal Pelajaran') ?></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
         <!-- ####################################################################################################### -->
         <div class="wrapper col3">
-            <div id="featured_slide">
-                <div id="featured_wrap">
-                    <h3>EDIT DATA JADWAL PELAJARAN</h3>
-                     <h1 align="center">Tabel Data Jadwal Pelajaran</h1>
-                        <table width="80%" border="1" cellpading="2">
-                            <thead>
-                                <tr>
-                                    <td><b>Kode Jadwal</b></td>
-                                    <td><b>NIP</b></td>
-                                    <td><b>ID Mapel</b></td>
-                                    <td><b>ID Kelas</b></td>
-                                    <td><b>Hari</b></td>
-                                    <td><b>Jam</b></td>
-                                    <td><b>Tahun Ajaran</b></td>
-                                </tr>
-                            </thead>
-                          
-                            <?php
-                           
-                            if (isset($jad)) {
-                                foreach ($jad->result() as $row) {
-                                    ?>
-                                    <tr>
-                                        <td><?php echo $row->kode_jadwal; ?></td>
-                                        <td><?php echo $row->id_mapel; ?></td>
-                                        <td><?php echo $row->nip; ?></td>
-                                        <td><?php echo $row->id_kelas; ?></td>
-                                        <td><?php echo $row->hari; ?></td>
-                                        <td><?php echo $row->jam; ?></td>
-                                        <td><?php echo $row->tahun_ajaran; ?></td>
-                                        <td><?php echo anchor('DataJadwalPelajaran/tampilFormEdit/' . $row->kode_jadwal, 'Edit'); ?></td>
-                                   
-                                    </tr>
-                                    <?php
-                                }
-                            }
-                            ?> 
-                        </table>
-
-                    </div>
-                </div>
-            </div>
+            <h3>EDIT DATA JADWAL PELAJARAN</h3>
+            <h1 align="center">Tabel Data Jadwal Pelajaran</h1>
+            <table width="80%" border="1" cellpading="2">
+                <thead>
+                    <tr>
+                        <td><b>Kode Jadwal</b></td>
+                        <td><b>NIP</b></td>
+                        <td><b>ID Mapel</b></td>
+                        <td><b>ID Kelas</b></td>
+                        <td><b>Hari</b></td>
+                        <td><b>Jam</b></td>
+                        <td><b>Tahun Ajaran</b></td>
+                    </tr>
+                </thead>
+                <?php
+                if (isset($ujadwal)) {
+                    foreach ($ujadwal as $row) {
+                        ?>
+                        <tr>
+                            <td><?php echo $row->kode_jadwal; ?></td>
+                            <td><?php echo $row->id_mapel; ?></td>
+                            <td><?php echo $row->nip; ?></td>
+                            <td><?php echo $row->id_kelas; ?></td>
+                            <td><?php echo $row->hari; ?></td>
+                            <td><?php echo $row->jam; ?></td>
+                            <td><?php echo $row->tahun_ajaran; ?></td>
+                            <td><?php echo anchor('DataJadwalPelajaran/tampilFormEdit/' . $row->kode_jadwal, 'Edit'); ?></td>        
+                        </tr>
+                        <?php
+                    }
+                }
+                ?> 
+            </table>
+        </div>
         <!-- ####################################################################################################### -->
         <div class="wrapper col5">
             <div id="footer">
-               <div id="newsletter">
+                <div id="newsletter">
                     <h2>PENCARIAN GOOGLE</h2>
                     <p>Untuk melakukan pencarian <a href="http://www.google.com/">click here &raquo;</a></p>
                 </div>
--- views/MasukDataJadwalPelajaran.php
+++ views/MasukDataJadwalPelajaran.php
@@ -24,7 +24,7 @@ and open the template in the editor.
                     <p>SMA Pangudi Luhur Yogyakarta</p>
                 </div>
                 <div class="fl_right">
-                    <p>Tel: xxxxx xxxxxxxxxx | Mail: info@domain.com |<?php echo anchor('Kelola/index/', 'LOGOUT'); ?>| </p>     </div>
+                    <p>Tel: xxxxx xxxxxxxxxx | Mail: info@domain.com |<?php echo anchor('Kelola/index/', 'LOGOUT'); ?>| </p>         </div>
                 <br class="clear" />
             </div>
         </div>
@@ -77,17 +77,18 @@ and open the template in the editor.
         <!-- ####################################################################################################### -->
         <div class="wrapper col3">
             <h2>TAMBAH DATA JADWAL PELAJAR</h2>
-            <form method="post" action="<?= base_url(); ?>index.php/DataJadwalPelajaran/updateDataJadwalPelajaran">
+            <form method="post" action="<?= base_url(); ?>index.php/DataJadwalPelajaran/insertDataJadwalPelajaran">
                 <table> 
                     <tr>
                         <td><label>Kode Jadwal &nbsp;&nbsp;</label></td>
-                        <td><input value="<?php echo $kode_jadwal; ?>" name="kode" type="text" class="textbox" readonly=""></td>
+                        <td><input name="kode" type="text" class="textbox"></td>
                     </tr>
                     <tr><td>NIP</td>
                         <td><select name="nip">
+
                                 <?php
-                                if (isset($tnip_jp)) {
-                                    foreach ($tnip_jp->result() as $row) {
+                                if (isset($inip_jp)) {
+                                    foreach ($inip_jp->result() as $row) {
                                         ?>
                                         <option value="<?php echo $row->nip; ?>"><?php echo $row->nip; ?></option>
                                         <?php
@@ -96,11 +97,13 @@ and open the template in the editor.
                                 ?>             
                             </select>                                        
                         </td></tr>
+
                     <tr><td>ID Mapel</td>
                         <td><select name="mapel">
+
                                 <?php
-                                if (isset($tmapel_jp)) {
-                                    foreach ($tmapel_jp->result() as $row) {
+                                if (isset($imapel_jp)) {
+                                    foreach ($imapel_jp->result() as $row) {
                                         ?>
                                         <option value="<?php echo $row->id_mapel; ?>"><?php echo $row->id_mapel; ?></option>
                                         <?php
@@ -111,10 +114,12 @@ and open the template in the editor.
                         </td></tr>
                     <tr><td>Kelas</td>
                         <td><select name="kelas">
+
                                 <?php
-                                if (isset($tkelas_jp)) {
-                                    foreach ($tkelas_jp->result() as $row) {
+                                if (isset($ikelas_jp)) {
+                                    foreach ($ikelas_jp->result() as $row) {
                                         ?>
+
                                         <option value="<?php echo $row->id_kelas; ?>"><?php echo $row->id_kelas; ?></option>
                                         <?php
                                     }
@@ -122,6 +127,7 @@ and open the template in the editor.
                                 ?>             
                             </select>                                        
                         </td></tr>
+
                     <tr>
                         <td><label>Hari &nbsp;&nbsp;</label></td>
                         <td><select name="hr">
@@ -135,15 +141,48 @@ and open the template in the editor.
                     </tr>
                     <tr>
                         <td><label>Jam &nbsp;&nbsp;</label></td>
-                        <td><input value="<?php echo $jam; ?>" name="jam" type="text" class="textbox"></td>
+                        <td><input name="jam" type="text" class="textbox"></td>
                     </tr>
+
                     <tr>
                         <td><label>Tahun Ajaran &nbsp;&nbsp;</label></td>
-                        <td><input value="<?php echo $tahun_ajaran; ?>" name="tahun" type="text" class="textbox"></td>
+                        <td><input name="tahun" type="text" class="textbox"></td>
                     </tr>
                 </table>
                 <input type="submit" value="Simpan">
             </form>
+            <h1 align="center">Tabel Data Jadwal Pelajaran</h1>
+            <table width="80%" border="1" cellpading="2">
+                <thead>
+                    <tr>
+                        <td><b>Kode Jadwal</b></td>
+                        <td><b>NIP</b></td>
+                        <td><b>ID Mapel</b></td>
+                        <td><b>ID Kelas</b></td>
+                        <td><b>Hari</b></td>
+                        <td><b>Jam</b></td>
+                        <td><b>Tahun Ajaran</b></td>
+                    </tr>
+                </thead>
+
+                <?php
+                if (isset($ijadwal)) {
+                    foreach ($ijadwal as $row) {
+                        ?>
+                        <tr>
+                            <td><?php echo $row->kode_jadwal; ?></td>
+                            <td><?php echo $row->id_mapel; ?></td>
+                            <td><?php echo $row->nip; ?></td>
+                            <td><?php echo $row->id_kelas; ?></td>
+                            <td><?php echo $row->hari; ?></td>
+                            <td><?php echo $row->jam; ?></td>
+                            <td><?php echo $row->tahun_ajaran; ?></td>
+                        </tr>
+                        <?php
+                    }
+                }
+                ?> 
+            </table>
         </div>
         <!-- ####################################################################################################### -->
         <div class="wrapper col5">
