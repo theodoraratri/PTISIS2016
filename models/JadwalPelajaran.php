0a8c49774fc9c5c222bd064a28fdb2b84aee55a2
nikomangasih <nikomangasih@gmail.com>
2016-05-18 09:12:08 +0700

    Add files via upload

--- models/JadwalPelajaran.php
+++ models/JadwalPelajaran.php
@@ -13,47 +13,47 @@
  */
 class JadwalPelajaran extends CI_Model {
 
-    
-    
-    
-      function getJadwal($kode) {
+    function getAllJadwal() {
+        $this->db->select('jp.kode_jadwal,jp.id_kelas,jp.id_mapel,m.nama_mapel,jp.nip,g.nama_guru,jp.hari,jp.jam,k.nama_kelas ');
+        $this->db->from('jadwalpelajaran jp');
+        $this->db->join('kelas k', 'k.id_kelas=jp.id_kelas', 'inner');
+        $this->db->join('mapel m', ' m.id_mapel=jp.id_mapel', 'inner');
+        $this->db->join('guru g', ' g.nip=jp.nip', 'inner');
+        $query = $this->db->get()->result();
+        return $query;
+    }
 
+    function getJadwal($kode) {
         $this->db->where('kode_jadwal', $kode);
         return $this->db->get('jadwalpelajaran');
-
-        //select guru berdasarkan id yang dimiliki
+        //select mapel berdasarkan id yang dimiliki
     }
-    function Insert($data) {
+
+    public function insertdatajadwalpelajaran($data) {
         $this->db->insert('jadwalpelajaran', $data);
     }
 
-    public function edit( $data) {
-         $this->db->where('kode_jadwal', $data['kode_jadwal']);
+    public function updatedatajadwalpelajaran($data) {
+        $this->db->where('kode_jadwal', $data['kode_jadwal']);
         $this->db->update('jadwalpelajaran', $data);
     }
 
-    function getAllJadwal() {
-        //select semua data yang ada pada table guru $this--->db->select("*");
-        $this->db->from("jadwalpelajaran");
-        return $this->db->get();
-    }
-
-    public function delete($kodeJadwal) {
+    public function deletedatajadwalpelajaran($kodeJadwal) {
         $this->db->delete('jadwalpelajaran', $kodeJadwal);
     }
 
     public function tampilNIP() {
-        $query = $this->db->query("select nip from GURU");
+        $query = $this->db->query("select nip,nama_guru from GURU");
         return $query;
     }
 
     public function tampilIdMapel() {
-        $query = $this->db->query("select id_mapel from mapel");
+        $query = $this->db->query("select id_mapel,nama_mapel from mapel");
         return $query;
     }
 
     public function tampildKelas() {
-        $query = $this->db->query("select id_kelas from kelas");
+        $query = $this->db->query("select id_kelas,nama_kelas from kelas");
         return $query;
     }
 
--- models/Rekap.php
+++ models/Rekap.php
@@ -13,29 +13,51 @@
  */
 class Rekap extends CI_Model {
 
-    public function tampilRekap($kelas, $tahun,$mapel) {
-        $this->db->select('k.nama_kelas,s.nama_siswa,a.status');
-        $this->db->from('jadwalpelajaran jp');
-        $this->db->join('kelas k', 'jp.id_kelas=k.id_kelas','inner');
-        $this->db->join('mapel m', 'm.id_mapel=jp.id_mapel','inner');
-        $this->db->join('siswa s', 's.id_kelas=jp.id_kelas','inner');
-         $this->db->join('absen a', 'a.no_induk=s.no_induk','inner');
-        
-        $this->db->where('jp.id_kelas',$kelas);
-        $this->db->where('jp.tahun_ajaran', $tahun);
-      
+    public function tampilRekapAbsen($kelas, $tahun) {
+        $this->db->select("a.no_induk,a.id_kelas,s.nama_siswa,k.nama_kelas,SUM(case when`status`='sakit' then 1 else 0 end)S,SUM(case when`status`='alfa' then 1 else 0 end)A,SUM(case when`status`='izin' then 1 else 0 end)I,SUM(case when`status`='hadir' then 1 else 0 end)H");
+        $this->db->from('absen a');
+        $this->db->join('siswa s', 's.no_induk=a.no_induk', 'inner');
+        $this->db->join('kelas k', 'k.id_kelas=a.id_kelas', 'inner');
+        //  $this->db->join('mapel m', 'm.id_mapel=a.id_mapel', 'inner');
+        $this->db->where('a.id_kelas', $kelas);
+        $this->db->where('a.tahun_ajaran', $tahun);
+        $this->db->group_by("no_induk,tahun_ajaran,id_kelas");
         $query = $this->db->get()->result();
         return $query;
     }
 
-    public function tampilTahunAjaran() {
-
-        $query = $this->db->query("select tahun_ajaran from jadwalpelajaran");
+    public function tampilRekapKemajuanKelas($bulan, $kls) {
+        $this->db->select(' m.nama_mapel,k.id_kelas,kls.nama_kelas,tgl,k.jumlah_kehadiran,k.materiYD,k.kegiatanBM');
+        $this->db->from('kemajuan_kelas k');
+        $this->db->join('mapel m', 'm.id_mapel=k.id_mapel', 'inner');
+        $this->db->join('kelas kls', 'kls.id_kelas=k.id_kelas', 'inner');
+        $this->db->like('tgl', $bulan, 'after');
+        $this->db->where('k.id_kelas', $kls);
+        $query = $this->db->get()->result();
         return $query;
     }
 
     public function tampildKelas() {
-        $query = $this->db->query("select id_kelas from kelas");
+        $query = $this->db->query("select id_kelas,nama_kelas from kelas");
+        return $query;
+    }
+
+    public function tampilRekapCoba($noInduk) {
+        $this->db->where('no_induk', $noInduk);
+        return $this->db->get('absen');
+    }
+
+//    public function ambiIdkelas($id) {
+//        $this->db->where('id_kelas', $id);
+//        return $this->db->get('kelas');
+//    }
+    public function ambiIdkelas($id) {
+        $this->db->select('g.nama_guru,k.nama_kelas');
+        $this->db->from('kelas k');
+
+        $this->db->join('guru g', 'g.nip=k.nip', 'inner');
+        $this->db->where('k.id_kelas', $id);
+        $query = $this->db->get()->result();
         return $query;
     }
 
